a359c03c4cf21df4718aee2ca993045f3588145d
bayusetiawan01 <61371193+bayusetiawan01@users.noreply.github.com>
2020-06-07 17:00:07 +0700

    Update master.blade.php

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -40,22 +40,14 @@
     <!-- Overlay For Sidebars -->
     <div class="overlay"></div>
     <!-- #END# Overlay For Sidebars -->
-    <!-- Search Bar -->
-    <div class="search-bar">
-        <div class="search-icon">
-            <i class="material-icons">search</i>
-        </div>
-        <input type="text" placeholder="START TYPING...">
-        <div class="close-search">
-            <i class="material-icons">close</i>
-        </div>
-    </div>
     <!-- #END# Search Bar -->
     <!-- Top Bar -->
-    <nav class="navbar">
-        <div class="navbar-header" style="padding-top: 0px; padding-bottom:0px; margin-bottom:0px">
-            <a href="javascript:void(0);" class="bars" style="top:0 !important"></a>
-            <a class="navbar-brand" href="/admin">Mipa Bersatu</a>
+    <nav class="navbar" style="background-color:firebrick">
+        <div class="container-fluid" style="display:block">
+            <div class="navbar-header" style="padding: 0px; padding-bottom:0px; margin-bottom:0px">
+                <a href="javascript:void(0);" class="bars" style="top:0 !important"></a>
+                <a class="navbar-brand" style="float:left" href="/admin">Mipa Bersatu</a>
+            </div>
         </div>
     </nav>
     <!-- #Top Bar -->
