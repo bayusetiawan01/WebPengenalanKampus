28eb89576b8bfeaa466f0bfc9b4f5d97fa359e42
bayusetiawan01 <61371193+bayusetiawan01@users.noreply.github.com>
2020-08-21 08:25:00 +0700

    Error Page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Mipa Bersatu</title>
+    <!-- Favicon-->
+    <link rel="icon" href="{{ asset('/images/icons/favicon.ico') }}" type="image/x-icon">
+    <!-- Custom CSS -->
+    <link href="{{ asset('/css/style.min.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <div class="error-box">
+            <div class="error-body text-center">
+                <h1 class="error-title text-danger">404</h1>
+                <h3 class="text-uppercase error-subtitle">Halaman Tidak Ditemukan !</h3>
+                <p class="text-muted m-t-30 m-b-30">Kamu menuliskan url yang salah.</p>
+                <a href="/login" class="btn btn-danger btn-rounded waves-effect waves-light m-b-40">Kembali</a>
+            </div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- All Required js -->
+    <!-- ============================================================== -->
+    <script src="{{ asset('/vendor/admin-jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{ asset('/js/app.js') }}"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugin js -->
+    <!-- ============================================================== -->
+    <script>
+        $('[data-toggle="tooltip"]').tooltip();
+        $(".preloader").fadeOut();
+    </script>
+</body>
+
+</html>
